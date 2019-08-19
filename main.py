404e11f615e2164ad0fecb2daace65b7abb73e1a
uPrin <503109@naver.com>
2019-08-19 20:17:09 +0900

    Adding min / max / avg tuple
    Adding min / max / avg tuple

--- main.py
+++ main.py
@@ -21,9 +21,19 @@ while True:
     #predict
     dlib_shape = predictor(img,face)
     shape_2d = np.array([[p.x , p.y] for p in dlib_shape.parts()])
+
+    #compute center and boundaris of face
+    top_left = np.min(shape_2d, axis=0)
+    bottom_right = np.max(shape_2d, axis=0)
+
+    center_x, center_y = np.mean(shape_2d, axis=0).astype(np.int)
+
     #visualize
     img = cv2.rectangle(img,pt1=(face.left(), face.top()), pt2=(face.right(),face.bottom()), color=(255,255,255),thickness=2, lineType=cv2.LINE_AA)
     for s in shape_2d:
         cv2.circle(img,center=tuple(s), radius=1,color=[255,255,255],thickness=2, lineType=cv2.LINE_AA)
+    cv2.circle(img,center=tuple(top_left), radius=1,color=[255,0,0],thickness=2, lineType=cv2.LINE_AA)
+    cv2.circle(img,center=tuple(bottom_right), radius=1,color=[255,0,0],thickness=2, lineType=cv2.LINE_AA)
+    cv2.circle(img,center=tuple((center_x, center_y)), radius=1,color=[0,0,255],thickness=2, lineType=cv2.LINE_AA)
     cv2.imshow('img',img)
     cv2.waitKey(1)
